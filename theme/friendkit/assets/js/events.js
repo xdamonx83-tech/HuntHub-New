7253d2b9c594625d6f42962151bfd110297dcc0e
xdamonx83-tech <xdamonx83@gmail.com>
2025-10-24 14:08:02 +0200

    Delete theme/friendkit directory

--- theme/friendkit/assets/js/events.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-/*! events.js | Friendkit | © Css Ninja. 2019-2020 */$(function(){$("#events-page").length&&$(".scroll-link").on("click",function(t){t.preventDefault(),$(this).siblings(".scroll-link").removeClass("is-active"),$(this).addClass("is-active");var s=$(this).attr("data-event-id");$("#event-list");$("html, body").animate({scrollTop:$("#"+s).offset().top-58},500)})});
--- theme/friendkit/partials/footer_fk.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer">
-  <div class="content has-text-centered">
-    <p>© <?= date('Y') ?> Hunthub — Friendkit</p>
-  </div>
-</footer>
